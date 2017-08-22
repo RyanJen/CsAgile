6e36422be00c6b3ba8cd1efe1e0e2a1e430af2a5
Ryan Jen <https://adszxc000@bitbucket.org/>
2017-08-22 15:54:07 +0800

    完成測試初始化

---
+++ CsAgile/Exceptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CsAgile
+{
+    public class ServiceExceptions : Exception, ISerializable
+    {
+        public ServiceExceptions()
+        : base("show message") { }
+        public ServiceExceptions(string message)
+        : base(message) { }
+        public ServiceExceptions(string message, Exception inner)
+        : base(message, inner) { }
+        protected ServiceExceptions(SerializationInfo info, StreamingContext context)
+        : base(info, context) { }
+    }
+
+    public class DomainException : Exception, ISerializable
+    {
+        //public DomainException()
+        //: base("show message") { }
+        //public DomainException(string message) 
+        //: base(message) { }
+        //public DomainException(string message, Exception inner) 
+        //: base(message, inner) { }
+        //protected DomainException(SerializationInfo info, StreamingContext context) 
+        //: base(info, context) { }
+    }
+}
