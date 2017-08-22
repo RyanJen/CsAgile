d753bb577ca35ac204bf23be6cd5edb31a7c400b
Ryan Jen <https://adszxc000@bitbucket.org/>
2017-08-22 15:55:33 +0800

    完成測試初始化

--- CsAgile/AccountService.cs
+++ CsAgile/AccountService.cs
@@ -6,13 +6,24 @@ namespace CsAgile
     {
         public AccountService(IAccountRepository repository)
         {
+            if (repository == null) throw new ArgumentNullException("repository", "A valid account repository must be supplied.");
             this.repository = repository;
         }
 
         public void AddTransactionToAccount(string uniqueAccountName, decimal transactionAmount)
         {
             var account = repository.GetByName(uniqueAccountName);
-            account.AddTransaction(transactionAmount);
+            if (account != null)
+            {
+                try
+                {
+                    account.AddTransaction(transactionAmount);
+                }
+                catch(DomainException domainException)
+                {
+                    throw new ServiceExceptions("An exception was throw by a domain object", domainException);
+                }
+            }
         }
 
         private readonly IAccountRepository repository;
--- CsAgileTest/AccountServiceTests.cs
+++ CsAgileTest/AccountServiceTests.cs
@@ -8,15 +8,27 @@ namespace CsAgileTest
     [TestClass]
     public class AccountServiceTests
     {
+        [TestInitialize]
+        public void Setup()
+        {
+            mockAccount = new Mock<Account>();
+            mockRepository = new Mock<IAccountRepository>();
+            sut = new AccountService(mockRepository.Object);
+        }
+
+        private Mock<Account> mockAccount;
+        private Mock<IAccountRepository> mockRepository;
+        private AccountService sut;
+
         [TestMethod]
         public void AddingTransactionToAccountDelegatesToAccountInstance()
         {
             //Arrange
             var account = new Account();
-            var mockRepository = new Mock<IAccountRepository>();
+            //var mockRepository = new Mock<IAccountRepository>();
             mockRepository.Setup(r => r.GetByName("Trading Account")).Returns(account);
             //var fakeRepository = new FakeAccountRepository(account);
-            var sut = new AccountService(mockRepository.Object);
+            //var sut = new AccountService(mockRepository.Object);
             //var sut = new AccountService(fakeRepository);
 
             //Act
@@ -24,6 +36,58 @@ namespace CsAgileTest
 
             //Assert
             Assert.AreEqual(200m, account.Balance);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CannotCreateAccountServiceWithNullAccountRepository()
+        {
+            //Arrange
+
+            //Act
+            new AccountService(null);
+            
+            //Assert
+        }
+
+        /// <summary>
+        /// 既沒有期望異常也沒有斷言的單元測試
+        /// </summary>
+        [TestMethod]
+        public void DoNotThrowWhenAccountIsNotFound()
+        {
+            //Arrange
+            //var mockRepository = new Mock<IAccountRepository>();
+            //var sut = new AccountService(mockRepository.Object);
+
+            //Act
+            sut.AddTransactionToAccount("Trading Account", 100m);
+
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ServiceExceptions))]
+        public void AccountExceptionAreWrappedInThrowServiceException()
+        {
+            //Arrange
+            //var account = new Mock<Account>();
+            mockAccount.Setup(a => a.AddTransaction(100m)).Throws<DomainException>();
+            //var mockRepository = new Mock<IAccountRepository>();
+            mockRepository.Setup(r => r.GetByName("Trading Account")).Returns(mockAccount.Object);
+            //var sut = new AccountService(mockRepository.Object);
+
+            //Act
+            try
+            {
+                sut.AddTransactionToAccount("Trading Account", 100m);
+            }
+            catch(ServiceExceptions serviceExceptions)
+            {
+                //Assert
+                Assert.IsInstanceOfType(serviceExceptions.InnerException, typeof(DomainException));
+            }
+
+            //Assert
         }
     }
 }
